d4f92809a625a87d3dbee011860227886498d57e
otap1211 <98668107+otap1211@users.noreply.github.com>
2022-02-20 23:49:57 -0300

    Update calva.js

--- calva.js
+++ calva.js
@@ -3,16 +3,20 @@ exports.run = async (client, message, args) => {
   
 const ultraCalvo = new Discord.MessageEmbed()
  .addField('SU PORCENTAJE DE CALVA ES DE...', `> %100\n\nLiteralmente es el lider calvo`)
- .setColor("RANDOM")
+ .setColor(0x255684)
   
 let porcentaje = ["10%", "12%", "14%", "16%", "18%", "20%", "22%", "25%", "26%", "27%", "28%", "31%", "32%","35%", "50%", "51%", "54%", "53%", "64%", "69%", "72", "78%", "85%", "91", "97", "100"]
 var calvo = porcentaje[Math.floor(Math.random() * porcentaje.length)]
 let userm = message.mentions.users.first() || message.author;
 
-  if(userm == "559502596847435827") {
+  if(userm == "800485642072752209") {
   return message.channel.send(ultraCalvo)
+} 
+
+if(userm == client.user.id) {
+return message.channel.send('yo ya soi calvo xd')
 } else {
- 
+  
 const CalvEmbed = new Discord.MessageEmbed()
  .addField(`EL PORCENTAJE DE CALVA DE ES DE...`, `> ${calvo}\n\n ${userm} esta calvo XD`)
  .setColor(0x255684)
